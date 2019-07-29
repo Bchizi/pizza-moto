22a376f334a29d635712510d9a70526ca884ec2e
Bchizi <leonbichanga@gmail.com>
2019-07-29 12:49:42 +0300

    edit form

--- js/script.js
+++ js/script.js
@@ -1,23 +1,23 @@
 // User interface
 // Displaying the ordering form
-function getSize() {
-    var selectedValue = document.getElementById("inputSize").value;
+function getSizeValue() {
+    var selectedValue = document.getElementById("size").value;
     return parseInt(selectedValue);
 }
 function getCrust() {
-    var selectedCrust = document.getElementById("inputcrust").value;
+    var selectedCrust = document.getElementById("crust").value;
     return parseInt(selectedCrust);
 }
 function getToppings() {
-    var selectedToppings = document.getElementById("inputtop").value;
+    var selectedToppings = document.getElementById("toppings").value;
     return parseInt(selectedToppings);
 }
 function getQuantity() {
-    var selectedQuantity = document.getElementById("quantity").value;
+    var selectedQuantity = document.getElementById("quan").value;
     return parseInt(selectedQuantity);
 }
 function getTotalAmount() {
-    var totalAmount = (getSize() + getCrust() + getToppings()) * getQuantity();
+    var totalAmount = (getSizeValue() + getCrust() + getToppings()) * getQuantity();
     alert("You ordered " + getQuantity() + " pizza(s)." + "" + " Your bill is " + (totalAmount));
 }
 //UI interface
